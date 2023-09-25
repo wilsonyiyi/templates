9f3e91155770aa65b17c669ee4453ca6c13a0e86
wangweiwei <will_workhard@163.com>
2023-09-25 20:13:56 +0800

    feat: 添加prettier配置

--- react-h5/src/app.tsx
+++ react-h5/src/app.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from 'react'
 
 interface IProps {}
 const App: React.FC<IProps> = ({}) => {
-  return <>app .tsx</>;
-};
+  return <>app .tsx</>
+}
 
-export default App;
+export default App
